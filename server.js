26f02c52fb47259040a049d4f8b34609d73b639f
Jefferson Lam <jeffersonwlam@gmail.com>
2015-08-05 14:20:52 -0500

    Changed Mongoose database and add Post route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+mongoose.connect('mongodb://admin:root@ds059672.mongolab.com:59672/restful-bears'); // connect to our database
 var Bear     = require('./app/models/bear');
 
 // configure app to use bodyParser()
@@ -36,6 +36,25 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        console.log('Attempting to create bear');
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+        console.log(bear);
+        // save the bear and check for errors
+        bear.save(function(err) {
+            console.log(err);
+            if (err) res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+
+    });
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
